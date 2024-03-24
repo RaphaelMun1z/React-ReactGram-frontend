1935b59c49b08660f109996aea90aa3f39f8a5ca
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-03-24 19:23:57 -0300

    Att

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api"
-export const uploads = "http://localhost:5000/uploads"
+export const api = "https://react-gram-backend-six.vercel.app/api"
+export const uploads = "https://react-gram-backend-six.vercel.app/uploads"
 
 export const requestConfig = (method, data, token = null, image = null) => {
     let config
